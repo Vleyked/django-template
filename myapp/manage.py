eedd00a778b487de669bf5bb5afb4c577eeb97b2
Victor Cid <52244754+Vleyked@users.noreply.github.com>
2024-02-05 01:24:25 +0000

    Fix Django settings module import

--- myapp/manage.py
+++ myapp/manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dinosaur_app.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinosaur_app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,9 +14,10 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+            "Hello there this is a test for the new branch."
         ) from exc
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
